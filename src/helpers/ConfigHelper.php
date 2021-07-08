3ee75cfee0834a91fc7c49f3f33c017e87b45d8a
Yoannis Jamar <office@yoannis.com>
2021-07-08 02:51:17 +0100

    api-v2: update output format constants

--- src/helpers/ConfigHelper.php
+++ src/helpers/ConfigHelper.php
@@ -31,10 +31,92 @@ class ConfigHelper
     // -------------------------------------------------------------------------
 
     /**
-     * List of source video file extensions
+     * List of format specs that are relevant for video encoding
      */
 
-    const SOURCE_FILE_EXTENSIONS = [
+    const VIDEO_SPEC_KEYS = [
+        'video_codec', 'resolution', 'video_bitrate', 'fps',
+        'pix_fmt', '2pass', 'frag', 'vprofile', 'level', 'quality', 'maxrate',
+    ];
+
+    /**
+     * List of format specs that are relevant for audio encoding
+     */
+
+    const AUDIO_SPEC_KEYS = [
+        'audio_codec', 'audio_bitrate', 'sample_rate', 'audio_channel',
+    ];
+
+    /**
+     * 
+     */
+
+    const PREVIEW_SPEC_KEYS = [
+        'resolution', 'pix_fmt', // '2pass',
+    ];
+
+    /**
+     * List of video codec spec values supported by the Coconut service
+     * @see https://docs.coconut.co/references/formats#basics
+     */
+
+    const VIDEO_CODECS = [
+        'mpeg4', 'xvid', 'flv', 'h263', 'mjpeg', 'mpeg1video', 'mpeg2video',
+        'qtrle', 'svq3', 'wmv1', 'wmv2', 'huffyuv', 'rv20', 'h264', 'hevc',
+        'vp8', 'vp9', 'theora', 'dnxhd', 'prores',
+    ];
+
+    /**
+     * 
+     */
+
+    const AUDIO_CODECS = [
+        'mp3', 'mp2', 'aac', 'amr_nb', 'ac3', 'vorbis', 'flac',
+        'pcm_u8', 'pcm_s16le', 'pcm_alaw', 'wmav2',
+    ];
+
+    /**
+     * Regex patterns used to identify specification values
+     */
+
+    // =common
+    const RESOLUTION_PATTERN = '/^\d+x\d+$/';
+    const RESOLUTION_DEFINITION_PATTERN = '/^(240|360p|480p|720p|1080p|2160p)p$/';
+
+    // =video
+    const VIDEO_BITRATE_PATTERN = '/^\d{2,6}k$/';
+    const FPS_PATTERN = '/^(0fps|15fps|23.98fps|25fps|29.97fps|30fps)$/';
+
+    // =audio
+    const AUDIO_BITRATE_PATTERN = '/^(32|64|96|128|160|192|224|256|288|320|352|384|416|448|480|512)k$/';
+    const SAMPLE_RATE_PATTERN = '/^(8000|11025|16000|22000|22050|24000|32000|44000|44100|48000)hz$/';
+    const AUDIO_CHANNEL_PATTERN = '/^(mono|stereo)$/';
+
+    // =options
+    const PIX_FMT_PATTERN = '/^(yuv420p|yuv422p)$/';
+    const VPROFILE_PATTERN = '/^(baseline|main|high|high10|high422|high444)$/';
+    const VPROFILE_PATTERN_PRORES = '/^[0-3]{1}$/';
+    const LEVEL_PATTERN = '/^(10|11|12|13|20|21|22|30|31|32|40|41|42|50|51)$/';
+    const QUALITY_PATTERN = '/^[1-5]{1}$/';
+
+    /**
+     * Map of container aliases and their target container
+     * @see https://docs.coconut.co/references/formats#basics
+     */
+
+    const CONTAINER_ALIASES = [
+        'divx' => 'avi',
+        'xvid' => 'avi',
+        'wmv' => 'asf',
+        'flash' => 'flv',
+        'theora' => 'ogv',
+    ];
+
+    /**
+     * List of source file containers (i.e. extensions) supported for input files
+     */
+
+    const INPUT_CONTAINERS = [
         'mp4', 'm4p', 'm4v',
         'webm', 
         'avi', 'avchd',
@@ -78,18 +160,52 @@ class ConfigHelper
     ];
 
     /**
+     * Default format specification values for given container
+     * @see https://docs.coconut.co/references/formats#basics
+     */
+
+    const CONTAINER_SPECS = [
+
+        // =defaults
+        '*' => [ // (value of container keys below gets merged in with these)
+            'resolution' => '0x0', 'video_bitrate' => '1000k', 'fps' => '0fps', // video
+            'audio_bitrate' => '44100hz', 'sample_rate' => '128k', 'audio_channel' => 'stereo', // audio
+            'pix_fmt' => null, '2pass' => false, // common options
+            'frag' => false, // mp4 options
+            'vprofile' => null, 'level' => null, // h264, hevc options ('vprofile' also for prores)
+            'quality' => null, 'maxrate' => null, // h264, hevc, vp8, vp9 options
+        ],
+
+        // =video
+        'mp4' => [ 'video_codec' => 'h264', 'audio_codec' => 'aac' ],
+        'webm' => [ 'video_codec' => 'vp8', 'audio_codec' => 'vorbis' ],
+        'avi' => [ 'video_codec' => 'mpeg4', 'audio_codec' => 'mp3' ],
+        'asf' => [ 'video_codec' => 'wmv2', 'audio_codec' => 'wmav2' ],
+        'mpegts' => [ 'video_codec' => 'h264', 'audio_codec' => 'aac' ],
+        'mov' => [ 'video_codec' => 'h364',  'audio_codec' => 'aac' ],
+        'flv' => [ 'video_codec' => 'flv',  'audio_codec' => 'mp3' ],
+        'mkv' => [ 'video_codec' => 'h264',  'audio_codec' => 'aac' ],
+        '3gp' => [ 'video_codec' => 'h263',  'audio_codec' => 'aac', 'sample_rate' => '32k' ],
+        'ogv' => [ 'video_codec' => 'theora',  'audio_codec' => 'vorbis' ],
+
+        // =audio
+        'ogg' => [ 'audio_codec' => 'vorbis' ],
+        'mp3' => [ 'audio_codec' => 'mp3' ],
+    ];
+
+    /**
      * Maps formatting video definitions to video specs
      */
 
-    const VIDEO_DEFINITION_SPECS = [
-        '240p' => [ 'resolution' => '0x240', 'aspect' => '*', 'bitrate' => '500K' ],
-        '360p' => [ 'resolution' => '0x360', 'aspect' => '*', 'bitrate' => '800K' ],
-        '480p' => [ 'resolution' => '0x480', 'aspect' => '*', 'bitrate' => '1000k' ],
-        '720p' => [ 'resolution' => '1280x720', 'aspect' => '16:9', 'bitrate' => '2000k' ],
-        '1080p' => [ 'resolution' => '1980x1080', 'aspect' => '16:9', 'bitrate' => '4000k' ],
-        '2160p' => [ 'resolution' => '3840x2160', 'aspect' => '16:9', 'bitrate' => '8000k' ],
+    const RESOLUTION_DEFINITION_SPECS = [
+        '240p' => [ 'resolution' => '0x240', 'video_bitrate' => '500K' ],
+        '360p' => [ 'resolution' => '0x360', 'video_bitrate' => '800K' ],
+        '480p' => [ 'resolution' => '0x480', 'video_bitrate' => '1000k' ],
+        '720p' => [ 'resolution' => '1280x720', 'video_bitrate' => '2000k' ],
+        '1080p' => [ 'resolution' => '1980x1080', 'video_bitrate' => '4000k' ],
+        '2160p' => [ 'resolution' => '3840x2160', 'video_bitrate' => '8000k' ],
     ];
-
+    
     /**
      * Regex patterns to parse file paths and urls
      */
